1b1782cf8404e6990a9fdf94374696112bbd4b52
CJace33 <s176047@UCS.ac.uk>
2017-01-28 20:55:39 +0000

    Added initital prototype

---
+++ Prototypes/Ripplefootie/Assets/Scripts/MovementControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+public class MovementControl : MonoBehaviour {
+    public float Angle;
+    public float Y;
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(transform.position.x, Y + Mathf.Sin(Angle)*0.2f, transform.position.z);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, Y + Mathf.Sin(Angle) * 0.2f, transform.position.z);
+        Angle += 0.1f;
+
+    }
+
+
+}
