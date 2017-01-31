ad35012cca721fc2a3b305cf431b6d8b4a63dbe7
CJace33 <s176047@UCS.ac.uk>
2017-01-31 21:48:34 +0000

    Added another prototype and GIF's

---
+++ Prototypes/WaterBalance/Assets/SpawnWater.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnWater : MonoBehaviour {
+
+    public GameObject Water;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            Instantiate(Water, new Vector3(0,1,1), Quaternion.identity);
+	}
+}
