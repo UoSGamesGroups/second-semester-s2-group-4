5fab50c713fb05550bb63d8a119a1e34ce82e861
CJace33 <s176047@UCS.ac.uk>
2017-02-03 14:29:28 +0000

    Attempted ripple code, unsuccessful :(

---
+++ Prototypes/Ripplefootie/Assets/RippleFloor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RippleFloor : MonoBehaviour {
+
+    // the length of segment (world space)
+    public float SegmentLength = 5;
+
+    // the segment resolution (number of horizontal points)
+    public int SegmentResolution = 32;
+
+    // the size of meshes in the pool
+    public int MeshCount = 4;
+
+    // the maximum number of visible meshes. Should be lower or equal than MeshCount
+    public int VisibleMeshes = 4;
+
+    // the prefab including MeshFilter and MeshRenderer
+    public MeshFilter SegmentPrefab;
+
+    // helper array to generate new segment without further allocations
+    private Vector3[] _vertexArray;
+
+    // the pool of free mesh filters
+    private List<MeshFilter> _freeMeshFilters = new List<MeshFilter>();
+
+
+
+    private void Awake()
+    {
+        // Create vertex array helper
+        _vertexArray = new Vector3[SegmentResolution * 2];
+
+        // Build triangles array. For all meshes this array always will
+        // look the same, so I am generating it once 
+        int iterations = _vertexArray.Length / 2 - 1;
+        var triangles = new int[(_vertexArray.Length - 2) * 3];
+
+        for (int i = 0; i < iterations; ++i)
+        {
+            int i2 = i * 6;
+            int i3 = i * 2;
+
+            triangles[i2] = i3 + 2;
+            triangles[i2 + 1] = i3 + 1;
+            triangles[i2 + 2] = i3 + 0;
+
+            triangles[i2 + 3] = i3 + 2;
+            triangles[i2 + 4] = i3 + 3;
+            triangles[i2 + 5] = i3 + 1;
+        }
+
+        // Create colors array. For now make it all white.
+        var colors = new Color32[_vertexArray.Length];
+        for (int i = 0; i < colors.Length; ++i)
+        {
+            colors[i] = new Color32(255, 255, 255, 255);
+        }
+
+        // Create game objects (with MeshFilter) instances.
+        // Assign vertices, triangles, deactivate and add to the pool.
+        for (int i = 0; i < MeshCount; ++i)
+        {
+            MeshFilter filter = Instantiate(SegmentPrefab);
+
+            Mesh mesh = filter.mesh;
+            mesh.Clear();
+
+            mesh.vertices = _vertexArray;
+            mesh.triangles = triangles;
+
+            filter.gameObject.SetActive(false);
+            _freeMeshFilters.Add(filter);
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
