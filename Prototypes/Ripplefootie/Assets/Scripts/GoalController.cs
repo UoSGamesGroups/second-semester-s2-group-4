c3cbed5dcd14cbad6429543a21c177103a778f5d
CJace33 <s176047@UCS.ac.uk>
2017-02-16 00:18:51 +0000

    Clerared up folders, re-did bridgefloor and fixed goals

---
+++ Prototypes/Ripplefootie/Assets/Scripts/GoalController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalController : MonoBehaviour {
+
+    //Score holders
+
+    //Holds the number of goals the red player has
+    public int redScore = 0;
+    //Holds the number of goals the blue player has
+    public int blueScore = 0;
+    //Holds the number of goals represented on the slider bar
+    public int sliderScore;
+    
+
+    public bool scoreChange = false;
+
+    public Slider scoreSlider;
+    
+    
+    // Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        scoreSlider.value = sliderScore;
+
+        if (scoreSlider.value == scoreSlider.minValue)
+        {
+            Debug.Log("Blue Wins");
+        }
+
+        if (scoreSlider.value == scoreSlider.maxValue)
+        {
+            Debug.Log("Red Wins");
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            scoreChange = true;
+            redScore += 1;
+        }
+
+	}
+
+    public void Reset()
+    {
+
+    }
+}
---
+++ Prototypes/Ripplefootie/Assets/Scripts/GoalDetection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalDetection : MonoBehaviour
+{
+
+    public bool isRedGoal;
+    GameObject GoalControl;
+    GoalController GoalControllerScript;
+
+    void Start()
+    {
+        GoalControl = GameObject.Find("Arena");
+        GoalControllerScript = GoalControl.GetComponent<GoalController>();
+
+    }
+
+    //Detect when a ball enters the goal
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            //Decision on which goal to add points to
+            if (isRedGoal)
+            {
+                GoalControllerScript.redScore += 1;
+                GoalControllerScript.sliderScore++;
+                Debug.Log("Red Goal!");
+            }
+            if (!isRedGoal)
+            {
+                GoalControllerScript.blueScore += 1;
+                GoalControllerScript.sliderScore--;
+                Debug.Log("Blue Goal!");
+
+            }
+            GoalControllerScript.Reset();
+        }
+    }
+}
