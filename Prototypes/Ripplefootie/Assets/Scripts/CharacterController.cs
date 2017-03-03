03da559c6a95d1d73b4100315c3d2b9e172e1603
CJace33 <s176047@UCS.ac.uk>
2017-03-03 14:18:54 +0000

    re-add audio with good folder name

---
+++ Prototypes/Ripplefootie/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Script to control the movement/jumping of the characters and the "kicking" mechanic
+
+public class CharacterController : MonoBehaviour
+{
+
+    //Bool to tell if the player is faceing right or left
+    private bool facingRight = false;
+    //Maximum speed the player can travel in the X axis
+    private float maxHorizontalSpeed = 10f;
+    //The amount of force added when the player jumps
+    public float jumpForce = 100f;
+    //Bool to determine if the player is red or not
+    public bool redPlayer;
+    //A position marking where to check if the player is grounded 
+    private Transform groundCheck;
+    //Bool to keep track of if the player is grounded
+    private bool grounded;
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
