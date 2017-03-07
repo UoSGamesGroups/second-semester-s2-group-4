a20c828888f1108d086e753b71366cf663519f3b
CJace33 <s176047@UCS.ac.uk>
2017-03-07 12:14:42 +0000

    Created character controller
    Jump doesn't work yet, goals also appear to bug often

--- Prototypes/Ripplefootie/Assets/Scripts/CharacterController.cs
+++ Prototypes/Ripplefootie/Assets/Scripts/CharacterController.cs
@@ -1,40 +1,60 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-//Script to control the movement/jumping of the characters and the "kicking" mechanic
+//For character movement, keys are (ideally)rebindable, so only one script is needed
+//isGrounded code: http://answers.unity3d.com/questions/149790/checking-if-grounded-on-rigidbody.html
 
 public class CharacterController : MonoBehaviour
 {
+    public KeyCode kcRight = KeyCode.RightArrow;
+    public KeyCode kcLeft = KeyCode.LeftArrow;
+    public KeyCode kcUp = KeyCode.UpArrow;
 
-    //Bool to tell if the player is faceing right or left
-    private bool facingRight = false;
-    //Maximum speed the player can travel in the X axis
-    private float maxHorizontalSpeed = 10f;
-    //The amount of force added when the player jumps
-    public float jumpForce = 100f;
-    //Bool to determine if the player is red or not
-    public bool redPlayer;
-    //A position marking where to check if the player is grounded 
-    private Transform groundCheck;
-    //Bool to keep track of if the player is grounded
-    private bool grounded;
+    //The amount of force applied per frame to the rigidbody
+    public float moveSpeed;
+    //The amount of force applied on a jump
+    public float jumpSpeed;
+    //A bool to check if the player is currently grounded
+    bool isGrounded = false;
+    private Rigidbody2D RigidB;
 
-
-
-
-
-
-
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        RigidB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
     {
-		
-	}
+        //Move Right
+        if (Input.GetKey(kcRight))
+        {
+            RigidB.AddForce(new Vector2(moveSpeed, 0), ForceMode2D.Force);
+        }
+        //Move Left
+        if (Input.GetKey(kcLeft))
+        {
+            RigidB.AddForce(new Vector2(-moveSpeed, 0), ForceMode2D.Force);
+        }
+        //Jump
+        if (Input.GetKeyDown(kcUp))
+        {
+            Debug.Log("Should be jumping");
+            RigidB.velocity = new Vector2(0, jumpSpeed);
+        }
+
+
+
+    }
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        isGrounded = false;
+    }
+
 }
